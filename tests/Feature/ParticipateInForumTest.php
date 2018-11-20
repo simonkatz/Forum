da7ff5bdf05ebd63dad3602c4c9c2bf7ef6e4ae6
Simon Kateregga <spkateregga@gmail.com>
2018-11-19 23:52:50 -0600

    Test Validation Errors

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -34,4 +34,19 @@ class ParticipateInForumTest extends TestCase
     $this->get($thread->path())
          ->assertSee($reply->body);
   }
+  
+  /** @test */
+  public function it_requires_a_body()
+  {
+      $this->expectException('Illuminate\Validation\ValidationException');
+      
+      $this->signIn();
+      
+      $thread = create('App\Thread');
+      
+      $reply = make('App\Reply', ['body' => null]);
+      
+      $this->post($thread->path() . '/replies', $reply->toArray())
+           ->assertSessionHasErrors('body');
+  }
 }
